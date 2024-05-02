8c21b1b1ffca9534a8550da094c6de4644b0c75d
Oliver Locke <oliver@lockenet.com.au>
2024-05-02 19:06:02 +1000

    Added bash language server for neovim

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = {"pyright", "clangd", "tsserver"}
+local servers = {"pyright", "clangd", "tsserver", "bashls"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
